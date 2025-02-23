fd6d038036d325b402f6fe488a7f8b546609c90b
Saeed Emami <s.emami@mobinnet.ir>
2025-02-23 19:13:24 +0330

    Create the RegisterTrip.tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import InputField from "./components/field/InputField";
+import RegisterTrip from "./pages/travel-registration/RegisterTrip";
 
 function App() {
-  const [name, setName] = useState("");
-
   return (
-    <div className="p-4">
-      <InputField label="نام" value={name} onChange={(e) => setName(e.target.value)} />
-      <p className="mt-2">مقدار وارد شده: {name}</p>
+    <div>
+      <RegisterTrip />
     </div>
   );
 }
---
+++ src/pages/travel-registration/RegisterTrip.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterTrip: React.FC = () => {
+  return <h1 className="text-xl font-bold p-4">صفحه‌ی ثبت سفر</h1>;
+};
+
+export default RegisterTrip;
