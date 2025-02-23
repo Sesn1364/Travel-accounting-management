1bc095455e026edec44281437beee7682e29def8
Saeed Emami <s.emami@mobinnet.ir>
2025-02-23 18:29:33 +0330

    Create a Travel accounting management project and install React, TypeScript, Vite, and Tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-200 text-2xl font-bold">
+      Tailwind CSS نصب شد! 🎉
+    </div>
+  );
+}
+
+export default App;
